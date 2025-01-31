ca8fb539813bb843044089f78a792459c5df8625
Rekimart <elarasoft.dev@gmail.com>
2025-01-31 08:48:48 -0800

    Change setup file

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="reki",
-    version="0.0.1",
-    packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "pandas"
-    ],
-    author="Rekimart",
-    author_email="support@rekimart.com",
-    description="A short description of your package",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/rekimart/rekimart",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-    ],
-    python_requires='>=3.7',
-)
+if __name__ == "__main__":
+    setup()
